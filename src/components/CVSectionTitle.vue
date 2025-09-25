7687143dbc984975bb1fa52ad505f8b0c6e69895
clemdee <clement@desableau.com>
2025-09-25 12:17:55 +0200

    Make section titles take all width

--- src/components/CVSectionTitle.vue
+++ src/components/CVSectionTitle.vue
@@ -1,10 +1,12 @@
 <template>
-  <h2 class="cv-section-title">
-    <CVAnchor :id="anchorId" />
-    <div class="title">
-      <slot />
-    </div>
-  </h2>
+  <div class="cv-section-title">
+    <h2>
+      <CVAnchor :id="anchorId" />
+      <div class="title">
+        <slot />
+      </div>
+    </h2>
+  </div>
 </template>
 
 <script lang="ts" setup>
@@ -16,38 +18,41 @@ defineProps<{
 </script>
 
 <style lang="scss" scoped>
-h2.cv-section-title {
-  position: relative;
-  display: inline-block;
-  font-size: 1rem;
-  break-after: avoid;
+.cv-section-title {
+  h2 {
+    position: relative;
+    display: flex;
+    font-size: 1rem;
+    break-after: avoid;
 
-  .title {
-    padding: 0.2rem 1rem;
-    margin-bottom: 1rem;
-    border-radius: 0.1rem;
-    font-family: 'roboto-thin';
-    font-weight: 800;
-    text-transform: uppercase;
-    letter-spacing: 0.08rem;
-    cursor: pointer;
-    transition: all 250ms;
+    .title {
+      width: 100%;
+      padding: 0.2rem 1rem;
+      margin-bottom: 1rem;
+      border-radius: 0.1rem;
+      font-family: 'roboto-thin';
+      font-weight: 800;
+      text-transform: uppercase;
+      letter-spacing: 0.08rem;
+      cursor: pointer;
+      transition: all 250ms;
 
-    border-left: 0.5rem solid var(--colorscheme-secondary);
-    color: var(--colorscheme-content-title-text);
-    background-color: var(--colorscheme-content-title-background)
-  }
+      border-left: 0.5rem solid var(--colorscheme-secondary);
+      color: var(--colorscheme-content-title-text);
+      background-color: var(--colorscheme-content-title-background)
+    }
 
-  .anchor {
-    position: absolute;
-    right: 100%;
-    top: 0.3rem;
-    padding-right: 0.4rem;
-  }
-
-  &:hover {
     .anchor {
-      opacity: 0.5;
+      position: absolute;
+      right: 100%;
+      top: 0.3rem;
+      padding-right: 0.4rem;
+    }
+
+    &:hover {
+      .anchor {
+        opacity: 0.5;
+      }
     }
   }
 }
