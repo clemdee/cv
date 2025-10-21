053882dac9bfdbd8f370db1baff5ea617d3bed89
clemdee <clement@desableau.com>
2025-10-21 12:26:55 +0200

    Fix cv last page height

--- src/components/CVContent.vue
+++ src/components/CVContent.vue
@@ -8,46 +8,66 @@
     <CVSectionSkills />
     <CVSectionEducation />
     <CVSectionHobbies />
+    <div
+      ref="pager"
+      class="pager"
+    />
   </div>
 </template>
 
 <script lang="ts" setup>
-import { ref, customRef } from 'vue';
-import { useResizeObserver } from '@vueuse/core'
-
+import { ref, useTemplateRef, onMounted } from 'vue';
 import CVHeader from '~/components/CVHeader.vue';
 import CVSectionEducation from '~/components/CVSectionEducation.vue';
 import CVSectionExperience from '~/components/CVSectionExperience.vue';
 import CVSectionHobbies from '~/components/CVSectionHobbies.vue';
 import CVSectionSkills from '~/components/CVSectionSkills.vue';
 
-const root = ref<HTMLElement | null>(null);
-const pageCount = customRef((track, trigger) => {
-  useResizeObserver(root, () => {
-    trigger();
-  });
-  return {
-    get () {
-      track();
-      return root.value?.getClientRects().length ?? 1;
-    },
-    set () {},
+const root = useTemplateRef('root');
+const pager = useTemplateRef('pager');
+
+const lastPageHeight = ref(0);
+
+const resizeObserver = new ResizeObserver(() => {
+  if (!root.value) return;
+  if (!pager.value) return;
+  pager.value.style.display = 'none';
+  const rects = root.value.getClientRects();
+  const height = rects.item(rects.length - 1)?.height;
+  lastPageHeight.value = height ?? 0;
+  pager.value.style.display = '';
+});
+
+onMounted(() => {
+  if (root.value) {
+    resizeObserver.observe(root.value);
+  }
+
+  // Workaround for firefox bug
+  // https://bugzilla.mozilla.org/show_bug.cgi?id=1832939
+  // ResizeObserver doesn't fire when element fills more than one column
+  const isFirefox = navigator.userAgent.search("Firefox") !== -1;
+  if (isFirefox) {
+    const innerElements = document.querySelectorAll('.cv-content > *, .cv-base-item');
+    innerElements.forEach((element) => {
+      resizeObserver.observe(element);
+    });
   }
 });
 </script>
 
 <style lang="scss" scoped>
-
 .cv-content {
-  --page-count: v-bind('pageCount');
+  --last-page-height: calc(v-bind('lastPageHeight') * 1px);
   min-height: var(--page-height);
-  height: calc(
-    var(--page-count) * var(--page-height)
-    - 4rem * (var(--page-count) - 1)
-    - 1px
-  );
   padding: 2rem;
   background-color: var(--colorscheme-content-background);
   color: var(--colorscheme-content-text);
+
+  .pager {
+    content: '';
+    display: block;
+    height: calc(var(--page-height) - var(--last-page-height));
+  }
 }
 </style>
